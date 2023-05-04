a24210b7448394c7328c1d667caac61ff1f85608
Samcolserra <samcolserra@gmail.com>
2023-05-04 21:45:55 +0300

    Initial commit. Added some page templates. Installed router deendency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Brewery from './pages/Brewery'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/brewery",
+        element: <Brewery />
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div>Whoops! Page not found.</div>
+    )
+}
+
+export default NotFound;
